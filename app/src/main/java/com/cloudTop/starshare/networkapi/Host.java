50a5ffba2375cf51513b43cb7e58a3f5294611c4
hankooxiaozei <1272937260@qq.com>
2017-09-15 18:27:07 +0800

    星云更改端口

--- app/src/main/java/com/cloudTop/starshare/networkapi/Host.java
+++ app/src/main/java/com/cloudTop/starshare/networkapi/Host.java
@@ -66,10 +66,10 @@ public enum Host {
 //        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //        if(BuildConfig.DEBUG){
 //            return "139.224.34.22"; //本地
-//            return "nsb.smartdata-x.com"; //测试
+            return "nsb.smartdata-x.com"; //测试
 //        }else{
 ////            return "tapi.smartdata-x.com";//线上正式
-            return "122.144.169.214"; //测试
+//            return "122.144.169.214"; //测试
 //        }
 
         //sdf
@@ -87,8 +87,9 @@ public enum Host {
 
 //         return (short) 16080;  //本地明星端
 //dsf
-//        return (short) 16016;  //明星端
-        return (short) 17004;//星云
+        return (short) 16016;  //明星端
+//        return (short) 17004;//星云
+
 //        return (short) 16080;
 
 //        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
